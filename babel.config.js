b59aa470f6580b9e1628365b92e214252d1478ea
Pedro Trindade <pedro.henrique@jobecam.com>
2025-04-09 09:20:06 -0300

    Corrigindo bugs

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,7 @@ module.exports = function (api) {
             assets: './src/assets',
             components: './src/components',
             requests: './src/requests',
+            utils: './src/utils',
           },
         },
       ],
